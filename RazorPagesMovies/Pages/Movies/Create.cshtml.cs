0a2358cb5c47595f782b38d1ac3ebb4ed094eeb7
VBTCAMYellow2019 <46923900+VBTCAMYellow2019@users.noreply.github.com>
2019-02-22 09:21:37 -0500

    init commit

---
+++ RazorPagesMovies/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Models;
+using RazorPagesMovies.Models;
+
+namespace RazorPagesMovies.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovies.Models.RazorPagesMoviesContext _context;
+
+        public CreateModel(RazorPagesMovies.Models.RazorPagesMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesMovies/RazorPagesMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie
+{
+    internal class RazorPagesMovieContext
+    {
+        private DbContextOptions<RazorPagesMovieContext> dbContextOptions;
+
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> dbContextOptions)
+        {
+            this.dbContextOptions = dbContextOptions;
+        }
+    }
+}
